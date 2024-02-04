11ec9c579aa3a0c9db499254623e496c33ddae52
Ícaro Forte <icarofort@gmail.com>
2024-02-04 16:34:09 -0300

    Redirecionando rotas erradas

--- utils/middleware.js
+++ utils/middleware.js
@@ -9,7 +9,8 @@ const requestLogger = (request, response, next) => {
 }
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
+  infoLog('Wrong URL', request.path)
+  response.status(301).redirect('/');
 }
 
 const errorHandler = (error, request, response, next) => {
